954b55c35f417ee6d271e9f61430e75474ee51d8
Wambui_Maina <you@example.com>
2022-09-22 11:20:50 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1*num2
+const random = num1
+Math.random(num1);
 
-// Write your code here
+const num3=24
+const num4=5
+const mod=num3%num4
+let max =20
+Math.max=(1,2,3,4,20);
